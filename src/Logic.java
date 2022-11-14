149ac30b9d3ca20dfe5ef6bacb4f045a9c15e3f0
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-11-14 16:49:35 +0300

    Logic added

---
+++ src/Logic.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Type;
+
+public class Logic
+{
+    private static Model3D model3D;
+    private static Redactor redactor;
+
+    private static Model3D modelTypeConvertation(Type inputModel)
+    {
+        // операции inputModel в  Model3D
+        return new Model3D();
+    }
+
+}

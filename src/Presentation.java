557900ea7d7316bc6050292ca0e5b6e167ebea10
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-11-14 16:35:43 +0300

    getters and setters added

--- src/Presentation.java
+++ src/Presentation.java
@@ -5,6 +5,22 @@ public class Presentation
     private static Model3D model3D;
     private static Redactor redactor;
 
+    public static void setModel3D(Model3D model3D) {
+        Presentation.model3D = model3D;
+    }
+
+    public static void setRedactor(Redactor redactor) {
+        Presentation.redactor = redactor;
+    }
+
+    public static Model3D getModel3D() {
+        return model3D;
+    }
+
+    public static Redactor getRedactor() {
+        return redactor;
+    }
+
     private static void observeModel(Model3D model)
     {
         // производим операции по обзору

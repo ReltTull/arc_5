9eefde28ebf5e72e169fb52dfba06397c61613ab
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-11-14 16:34:34 +0300

    Redactor descriped

--- src/Redactor.java
+++ src/Redactor.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Redactor {
+public class Redactor
+{
+    private static Model3D model3D;
+
+    public static void setModel3D(Model3D model3D) {
+        Redactor.model3D = model3D;
+    }
+
+    public static Model3D getModel3D() {
+        return model3D;
+    }
+
+    private static Model3D redactModel(Model3D model3D)
+    {
+        // операции с моделью 3д
+        return model3D;
+    }
 }

09f2f0669573f3b6db98579a0540bc6783b6b116
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-11-14 16:37:19 +0300

    getter and setter added

--- src/Model3D.java
+++ src/Model3D.java
@@ -3,6 +3,15 @@ import java.lang.reflect.Type;
 public class Model3D
 {
     private static Type inputParametr;
+
+    public static void setInputParametr(Type inputParametr) {
+        Model3D.inputParametr = inputParametr;
+    }
+
+    public static Type getInputParametr() {
+        return inputParametr;
+    }
+
     private static Model3D createModel3D()
     {
         return new Model3D();

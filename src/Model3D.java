f0543ec4b3cb9883b741fabdace2a32c7cbc5773
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-11-14 16:22:21 +0300

    Model compl

---
+++ src/Model3D.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Type;
+
+public class Model3D
+{
+    private static Type inputParametr;
+    private static Model3D createModel3D()
+    {
+        return new Model3D();
+    }
+}

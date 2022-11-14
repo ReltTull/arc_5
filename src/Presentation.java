d37f9e779cf154c51fd10af73b32dc92ade08cd7
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-11-14 16:31:06 +0300

    Presentation added

---
+++ src/Presentation.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+
+public class Presentation
+{
+    private static Model3D model3D;
+    private static Redactor redactor;
+
+    private static void observeModel(Model3D model)
+    {
+        // производим операции по обзору
+    }
+
+    private static void loadModel(Model3D model3D, Connection connection)
+    {
+        // операции с БД
+    }
+}
